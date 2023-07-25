609f613dd63a71fddc292e1a61c0113953f648e8
Mai <maiabdelhameed16@gmail.com>
2023-07-25 13:35:42 +0300

    initial unit testing

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+//    @Test
+//    void foo() {
+//        Item[] items = new Item[] { new Item("foo", 0, 0) };
+//        GildedRose app = new GildedRose(items);
+//        app.updateQuality();
+//        assertEquals("fixme", app.items[0].name);
+//    }
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void checkAgedBrie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 0 )};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(2, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
     }
 
+
+    @Test
+    void checkAgedBrieQualityEquals50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 50 )};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
 }
